9738f4b893cd3e97066dd5b13b81b193e82b435b
Punamchand <chavhanpunamchand@gmail.com>
2020-08-21 23:12:01 +0530

    21/08/Practice

---
+++ Interview_Program_Practice/String/Num__Pattern.py
@@ -0,0 +1,22 @@
+'''
+
+Num Pattern :-
+1                  1
+1 2                2 2
+1 2 3              3 3 3
+1 2 3 4            4 4 4 4
+
+
+for i in range(1,6):
+    for k in range(1,i):
+        print(k,end=" ")
+    print(" ")
+
+print("\n")
+'''
+
+for i in range(1,5):
+    for j in range(i):
+        print(i, end=" ")
+    print(" ")
+

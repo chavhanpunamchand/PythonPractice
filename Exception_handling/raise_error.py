d21c7d580650cee4b986508d52caf9498dc373bf
Punamchand <chavhanpunamchand@gmail.com>
2021-02-24 20:29:05 +0530

    Exception_Handling_done

---
+++ Exception_handling/raise_error.py
@@ -0,0 +1,10 @@
+try:
+     num = int(input("Enter a positive integer: "))
+     if(num <= 0):
+# we can pass the message in the raise statement
+         raise ValueError("That is  a negative number!")
+except ValueError as e:
+     print(e)
+
+else:
+    print("Your number is positive :",num)
--- Exception_handling/sample1.py
+++ Exception_handling/sample1.py
@@ -1,3 +1,32 @@
+"""
+Python facilitates us to not specify the exception with the except statement.
+
+We can declare multiple exceptions in the except statement
+ since the try block may contain the statements which throw the different type of exceptions.
+
+We can also specify an else block along with the try-except statement,
+ which will be executed if no exception is raised in the try block.
+ 
+The statements that don't throw the exception should be placed inside the else block.
+"""
+
+
+
+import sys
+sys.exit(0)
+
+
+import time
+
+time.sleep(5)
+try:
+    raise KeyboardInterrupt
+finally:
+    print('welcome, world!')
+
+
+
+
 
 def two_num_division(x,y):
     try:

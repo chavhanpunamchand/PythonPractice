e40418b1ca4a1d1f6637f54faac9971111011f70
Punamchand <chavhanpunamchand@gmail.com>
2021-02-24 09:44:24 +0530

    loops_completed

---
+++ patterns/star_pattern.py
@@ -0,0 +1,23 @@
+"""
+pattern writing
+
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*
+
+"""
+
+rows = int(input("Enter the rows: "))
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=" ")
+    print("\r")
+
+for i in range(rows - 1, 0, -1):
+    for j in range(0, i, 1):
+        print("*", end=" ")
+    print("\r")
